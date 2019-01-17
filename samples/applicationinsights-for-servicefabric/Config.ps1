670faba326063437c61cc64996884d145863a6ad
Christoph Petersen <cp@peterschen.de>
2019-01-17 10:26:15 +0100

    FIxed DSC config to install .NET core runtime

--- samples/applicationinsights-for-servicefabric/Config.ps1
+++ samples/applicationinsights-for-servicefabric/Config.ps1
@@ -13,20 +13,21 @@ configuration Config
             DestinationPath = "C:\tools"
         }
 
-        xRemoteFile "dotnet-install.ps1"
+        xRemoteFile "dotnet-hosting.exe"
         {
-            Uri = "https://dot.net/v1/dotnet-install.ps1"
+            Uri = "https://download.visualstudio.microsoft.com/download/pr/5ee633f2-bf6d-49bd-8fb6-80c861c36d54/caa93641707e1fd5b8273ada22009246/dotnet-hosting-2.2.1-win.exe"
             DestinationPath = "c:\tools"
             DependsOn = "[File]tools"
         }
 
-        ScheduledTask "dotnet"
+        Package "dotnet"
         {
-            TaskName = "dotnet"
-            TaskPath = '\Application Insights for Service Fabric'
-            ActionExecutable = "C:\tools\dotnet-install.ps1"
-            ActionArguments = "-Channel LTS"
-            ScheduleType = "Once"
+            Ensure = "Present"
+            Name = "Microsoft .NET Core Runtime - 2.2.1 (x64)"
+            ProductId = "588AB6F9-94E8-4909-B84B-0D69DFC1216C"
+            Arguments = "/quiet"
+            Path = "C:\tools\dotnet-hosting-2.2.1-win.exe"
+            DependsOn = "[xRemoteFile]dotnet-hosting.exe"
         }
     }
 }
