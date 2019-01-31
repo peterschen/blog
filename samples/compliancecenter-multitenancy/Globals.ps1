1230ae9e6d915ef64740c7c78e5726e8bc59fc73
Christoph Petersen <cp@peterschen.de>
2019-01-31 22:10:36 +0100

    Removed powershell scripts

--- samples/compliancecenter-multitenancy/Globals.ps1
+++
@@ -1,5 +0,0 @@
-$Global:ClientId = $env:SCCMT_CLIENTID;
-$Global:ClientSecret = $env:SCCMT_CLIENTSECRET;
-$Global:TenantDomain = $env:SCCMT_TENANTDOMAIN;
-$Global:TenantId = $env:SCCMT_TENANTID;
-$Global:Entities = $env:PROCESSOR_ENTITIES | ConvertFrom-Json;
