e811cbaf3427187315efefcd7aafdaa2a743d321
Christoph Petersen <cp@Christophs-MacBook-Pro.local>
2019-04-26 20:35:25 +0200

    Updated shell script to omit errors when running for a second time

--- samples/hunting-threats-with-asc/cs-victim.sh
+++ samples/hunting-threats-with-asc/cs-victim.sh
@@ -2,8 +2,28 @@
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update && apt-get install -y auditd
-useradd  -g users -m -p $(openssl passwd -1 password) -s /bin/bash admin
-useradd  -g users -m -p $(openssl passwd -1 princess) -s /bin/bash office
-useradd  -g users -m -p $(openssl passwd -1 abc123) -s /bin/bash John
-useradd  -g users -m -p $(openssl passwd -1 iloveyou) -s /bin/bash Jane
-useradd  -g users -m -p $(openssl passwd -1 qwerty) -s /bin/bash Susan
+
+exists=$(id -u admin > /dev/null 2>&1; echo $?)
+if [ $exists -eq 1 ]; then
+    useradd  -g users -m -p $(openssl passwd -1 password) -s /bin/bash admin
+fi
+
+exists=$(id -u office > /dev/null 2>&1; echo $?)
+if [ $exists -eq 1 ]; then
+    useradd  -g users -m -p $(openssl passwd -1 princess) -s /bin/bash office
+fi
+
+exists=$(id -u John > /dev/null 2>&1; echo $?)
+if [ $exists -eq 1 ]; then
+    useradd  -g users -m -p $(openssl passwd -1 abc123) -s /bin/bash John
+fi
+
+exists=$(id -u Jane > /dev/null 2>&1; echo $?)
+if [ $exists -eq 1 ]; then
+    useradd  -g users -m -p $(openssl passwd -1 iloveyou) -s /bin/bash Jane
+fi
+
+exists=$(id -u Susan > /dev/null 2>&1; echo $?)
+if [ $exists -eq 1 ]; then
+    useradd  -g users -m -p $(openssl passwd -1 qwerty) -s /bin/bash Susan
+fi
