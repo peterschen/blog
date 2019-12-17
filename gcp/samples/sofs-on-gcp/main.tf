8febde482b7a288e1abedc016d7c20404f24eb74
Christoph Petersen <cbpetersen@google.com>
2019-12-17 16:25:07 +0100

    Refactored SOFS deployment

--- gcp/samples/sofs-on-gcp/main.tf
+++ gcp/samples/sofs-on-gcp/main.tf
@@ -1,19 +1,16 @@
 provider "google" {
   version = "~> 3.1"
   project = "${var.project}"
-  region = "${var.region}"
-  zone = "${var.zone}"
 }
 
 provider "google-beta" {
   version = "~> 3.1"
   project = "${var.project}"
-  region = "${var.region}"
-  zone = "${var.zone}"
 }
 
 locals {
   name-sample = "sofs-on-gcp"
+  #count-instances = 3
 }
 
 data "google_client_config" "current" {}
@@ -21,47 +18,19 @@ data "google_client_config" "current" {}
 module "ad-on-gcp" {
   source = "github.com/peterschen/blog/gcp/samples/ad-on-gcp"
   project = var.project
+  regions = var.regions
+  zones = var.zones
   name-domain = var.name-domain
   password = var.password
 }
 
-resource "google_compute_instance" "sofs-primary" {
-   name         = "sofs-1"
-   machine_type = "n1-standard-2"
+resource "google_compute_instance" "sofs" {
+  count = 3
+  zone = "${var.regions[0]}-${var.zones[count.index]}"
+  name = "sofs-${count.index}"
+  machine_type = "n1-standard-2"
 
-  tags = ["sample-${local.name-sample}-sofs-1", "rdp"]
-
-  boot_disk {
-    initialize_params {
-      image = "windows-cloud/windows-2019"
-    }
-  }
-
-  network_interface {
-    network = module.ad-on-gcp.network
-    subnetwork = module.ad-on-gcp.subnet
-  }
-
-  metadata = {
-    sample                        = local.name-sample
-    type                          = "sofs"
-    sysprep-specialize-script-ps1 = templatefile("${module.ad-on-gcp.path-module}/specialize.ps1", { 
-        nameHost = "sofs-1", 
-        nameDomain = var.name-domain,
-        nameConfiguration = "sofs-primary",
-        uriMeta = var.uri-meta,
-        uriConfigurations = var.uri-configurations,
-        password = var.password 
-      })
-  }
-}
-
-resource "google_compute_instance" "sofs-secondaries" {
-   count = 2
-   name         = "sofs-${count.index + 2}"
-   machine_type = "n1-standard-2"
-
-  tags = ["sample-${local.name-sample}-sofs-${count.index + 2}", "rdp"]
+  tags = ["rdp"]
 
   boot_disk {
     initialize_params {
@@ -71,21 +40,22 @@ resource "google_compute_instance" "sofs-secondaries" {
 
   network_interface {
     network = module.ad-on-gcp.network
-    subnetwork = module.ad-on-gcp.subnet
+    subnetwork = module.ad-on-gcp.subnets[0]
   }
 
   metadata = {
-    sample                        = local.name-sample
-    type                          = "sofs"
+    sample = local.name-sample
+    type = "sofs"
     sysprep-specialize-script-ps1 = templatefile("${module.ad-on-gcp.path-module}/specialize.ps1", { 
-        nameHost = "sofs-${count.index + 2}", 
-        nameDomain = var.name-domain,
-        nameConfiguration = "sofs-secondaries",
+        nameHost = "sofs-${count.index}", 
+        nameConfiguration = "sofs",
         uriMeta = var.uri-meta,
         uriConfigurations = var.uri-configurations,
-        password = var.password 
+        password = var.password,
+        parametersConfiguration = jsonencode({
+          domainName = var.name-domain,
+          isFirst = (count.index == 0)
+        })
       })
   }
-
-  depends_on = [google_compute_instance.sofs-primary]
 }
