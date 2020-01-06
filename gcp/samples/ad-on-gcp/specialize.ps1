0f7f154d8e37430f2cf6ec5a048bdcf0eee4d134
Christoph Petersen <cp@peterschen.de>
2020-01-06 17:35:31 +0100

    Update xFailoverCluster DSC module (#39)

--- gcp/samples/ad-on-gcp/specialize.ps1
+++ gcp/samples/ad-on-gcp/specialize.ps1
@@ -57,8 +57,8 @@ $modules = @(
     },
     @{
         Name = "xFailOverCluster"
-        Version = "1.12.0.0"
-        Uri = "https://github.com/dsccommunity/xFailOverCluster/archive/1.12.0.0-PSGallery.zip"
+        Version = "1.13.0.0"
+        Uri = "https://github.com/dsccommunity/xFailOverCluster/archive/v1.13.0.zip"
     }
 );
 
