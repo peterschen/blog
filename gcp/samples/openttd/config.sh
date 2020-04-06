4ddeb86df397c5113e528e62ace45e2cf2229c10
Christoph Petersen <cp@peterschen.de>
2020-04-06 13:10:18 +0200

    Added container files

---
+++ gcp/samples/openttd/config.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env sh
+
+set +ex
+
+CONFIG=/home/openttd/.openttd/openttd.cfg
+
+# Default config values
+MAP_X=${MAP_X:=10}
+MAP_Y=${MAP_Y:=10}
+GENERATION_SEED=${GENERATION_SEED:=658464965}
+
+replace_setting()
+{
+    _setting=$1
+    _value=$2
+    _config=$3
+
+    sed -i "s/## $_setting ##/$_setting = $_value/" "$_config"
+}
+
+if [ $SERVER_PASSWORD != "" ]; then 
+    replace_setting "server_password" "$SERVER_PASSWORD" $CONFIG
+fi
+
+if [ $RCON_PASSWORD != "" ]; then 
+    replace_setting "rcon_password" "$RCON_PASSWORD" $CONFIG
+fi
+
+if [ $ADMIN_PASSWORD != "" ]; then 
+    replace_setting "admin_password" "$ADMIN_PASSWORD" $CONFIG
+fi
+
+# Generation seed
+replace_setting "map_x" "$MAP_X" $CONFIG
+replace_setting "map_y" "$MAP_Y" $CONFIG
+replace_setting "generation_seed" "$GENERATION_SEED" $CONFIG
+
+# Execute cmd from base image
+exec /openttd.sh
