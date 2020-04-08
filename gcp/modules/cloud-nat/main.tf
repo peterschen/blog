ea4dc6ae5f1a6abd49d86fdb31c760cec8d09af0
Christoph Petersen <cp@peterschen.de>
2020-04-08 14:16:49 +0200

    Explicit region values per resource

--- gcp/modules/cloud-nat/main.tf
+++ gcp/modules/cloud-nat/main.tf
@@ -1,20 +1,20 @@
 provider "google" {
   version = "~> 3.4"
-  region = var.region
 }
 
 locals {
-  project = var.project
   region = var.region
   network = var.network
 }
 
 resource "google_compute_router" "router" {
+  region = local.region
   name = "router"
   network = local.network
 }
 
 resource "google_compute_router_nat" "nat" {
+  region = local.region
   name = "nat"
   router = google_compute_router.router.name
   nat_ip_allocate_option = "AUTO_ONLY"
--- gcp/modules/cloud-nat/variables.tf
+++ gcp/modules/cloud-nat/variables.tf
@@ -1,8 +1,3 @@
-variable "project" {
-    type = string
-    default = ""
-}
-
 variable "region" {
     type = string
     default = ""
