dec68ed2929d67ea3ac455bc1aec0e65479e010d
Christoph Petersen <cp@peterschen.de>
2020-04-08 15:48:04 +0200

    Cloud NAT Terraform module (#87)
    * Add Cloud NAT module
    
    * Update project and region default values
    
    * Explicit region values per resource
    
    * Removed default variable values
    
    * Pass project to resources

---
+++ gcp/modules/cloud-nat/main.tf
@@ -0,0 +1,25 @@
+provider "google" {
+  version = "~> 3.4"
+}
+
+locals {
+  project = var.project
+  region = var.region
+  network = var.network
+}
+
+resource "google_compute_router" "router" {
+  project = local.project
+  region = local.region
+  name = "router"
+  network = local.network
+}
+
+resource "google_compute_router_nat" "nat" {
+  project = local.project
+  region = local.region
+  name = "nat"
+  router = google_compute_router.router.name
+  nat_ip_allocate_option = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
