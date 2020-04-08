3e52cb27ba5b1fc05ccf1e9f7e4a0d5d87b2c961
Christoph Petersen <cp@peterschen.de>
2020-04-08 18:02:12 +0200

    Firewall IAP TF module (#89)
    * Module to add RDP/SSH IAP firewall rules
    
    * Removed region parameter
    
    * Removed region from locals

---
+++ gcp/modules/firewall-iap/main.tf
@@ -0,0 +1,50 @@
+provider "google" {
+  version = "~> 3.4"
+}
+
+locals {
+  project = var.project
+  network = var.network
+  enable-rdp = var.enable-rdp
+  enable-ssh = var.enable-ssh
+}
+
+resource "google_compute_firewall" "allow-rdp-iap" {
+  count = local.enable-rdp ? 1 : 0
+  project = local.project
+
+  name    = "allow-rdp-iap"
+  network = local.network.name
+  priority = 5000
+
+  allow {
+    protocol = "tcp"
+    ports    = ["3389"]
+  }
+
+  direction = "INGRESS"
+
+  source_ranges = ["35.235.240.0/20"]
+
+  target_tags = ["rdp"]
+}
+
+resource "google_compute_firewall" "allow-ssh-iap" {
+  count = local.enable-ssh ? 1 : 0
+  project = local.project
+  
+  name    = "allow-ssh-iap"
+  network = local.network.name
+  priority = 5000
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  direction = "INGRESS"
+
+  source_ranges = ["35.235.240.0/20"]
+
+  target_tags = ["ssh"]
+}
---
+++ gcp/modules/firewall-iap/variables.tf
@@ -0,0 +1,16 @@
+variable "project" {
+    type = string
+}
+
+variable "network" {
+}
+
+variable "enable-rdp" {
+    type = bool
+    default = true
+}
+
+variable "enable-ssh" {
+    type = bool
+    default = true
+}
