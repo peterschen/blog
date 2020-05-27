95f12c1c5dda3da35c3016eb2570d6dbf7773052
Christoph Petersen <cp@peterschen.de>
2020-05-27 15:30:03 +0200

    Removed default values for regions and zones

--- gcp/modules/activedirectory/variables.tf
+++ gcp/modules/activedirectory/variables.tf
@@ -3,15 +3,10 @@ variable "project" {
 
 variable "regions" {
   type = list(string)
-  default = ["europe-west1", "europe-west4"]
 }
 
 variable "zones" {
   type = list(string)
-  default = [
-    "europe-west1-b",
-    "europe-west4-a"
-  ]
 }
 
 variable "network" {
