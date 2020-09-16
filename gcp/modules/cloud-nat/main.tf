a2fed4db13f73d4e508c6d957785e3190ce8c5a3
Christoph Petersen <cp@peterschen.de>
2020-09-16 17:52:42 +0200

    Removed project variable; added provider config

--- gcp/modules/cloud-nat/main.tf
+++ gcp/modules/cloud-nat/main.tf
@@ -1,18 +1,15 @@
 locals {
-  project = var.project
   region = var.region
   network = var.network
 }
 
 resource "google_compute_router" "router" {
-  project = local.project
   region = local.region
   name = "router"
   network = local.network.self_link
 }
 
 resource "google_compute_router_nat" "nat" {
-  project = local.project
   region = local.region
   name = "nat"
   router = google_compute_router.router.name
