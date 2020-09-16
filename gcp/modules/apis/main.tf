2ff7bceb15ebdc89c253713c77adf1e17438ba5a
Christoph Petersen <cp@peterschen.de>
2020-09-16 17:32:31 +0200

    Removed project variable

--- gcp/modules/apis/main.tf
+++ gcp/modules/apis/main.tf
@@ -1,10 +1,8 @@
 
 locals {
-  project = var.project
 }
 
 resource "google_project_service" "apis" {
-  project = local.project
   count = length(var.apis)
   service = var.apis[count.index]
   disable_dependent_services = true
--- gcp/modules/apis/variables.tf
+++ gcp/modules/apis/variables.tf
@@ -1,7 +1,3 @@
-variable "project" {
-    default = ""
-}
-
 variable "apis" {
     type = list(string)
     default = ["cloudresourcemanager.googleapis.com", "compute.googleapis.com"]
