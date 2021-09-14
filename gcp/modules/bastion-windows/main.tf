9fb24a3c2fe5c6c94983d6316da1f12cf786ff82
Christoph Petersen <cbpetersen@google.com>
2021-09-14 08:39:08 +0200

    Refactored internal variables and descriptors to camelCase

--- gcp/modules/bastion-windows/main.tf
+++ gcp/modules/bastion-windows/main.tf
@@ -5,13 +5,13 @@ locals {
   network = var.network
   subnetwork = var.subnetwork
   password = var.password
-  machine-type = var.machine-type
-  machine-name = var.machine-name
-  name-domain = var.domain-name
-  enable-domain = var.enable-domain
-  enable-ssms = var.enable-ssms
-  enable-hammerdb = var.enable-hammerdb
-  enable-diskspd = var.enable-diskspd
+  machineType = var.machine-type
+  machineName = var.machine-name
+  nameDomain = var.domain-name
+  enableDomain = var.enable-domain
+  enableSsms = var.enable-ssms
+  enableHammerdb = var.enable-hammerdb
+  enableDiskspd = var.enable-diskspd
 }
 
 data "google_compute_network" "network" {
@@ -25,26 +25,23 @@ data "google_compute_subnetwork" "subnetwork" {
   name = local.subnetwork
 }
 
-module "gce-default-scopes" {
-  # source = "github.com/peterschen/blog//gcp/modules/gce-default-scopes"
+module "gceDefaultScopes" {
   source = "../gce-default-scopes"
 }
 
 module "sysprep" {
-  # source = "github.com/peterschen/blog//gcp/modules/sysprep"
   source = "../sysprep"
 }
 
 module "apis" {
-  # source = "github.com/peterschen/blog//gcp/modules/apis"
   source = "../apis"
   apis = ["cloudresourcemanager.googleapis.com", "compute.googleapis.com"]
 }
 
 resource "google_compute_instance" "bastion" {
   zone = local.zone
-  name = local.machine-name
-  machine_type = local.machine-type
+  name = local.machineName
+  machine_type = local.machineType
 
   tags = ["bastion-windows", "rdp"]
 
@@ -68,22 +65,22 @@ resource "google_compute_instance" "bastion" {
 
   metadata = {
     sysprep-specialize-script-ps1 = templatefile(module.sysprep.path-specialize-nupkg, { 
-      nameHost = local.machine-name, 
+      nameHost = local.machineName, 
       password = local.password,
       parametersConfiguration = jsonencode({
         inlineMeta = filebase64(module.sysprep.path-meta),
         inlineConfiguration = filebase64("${path.module}/bastion.ps1"),
-        nameDomain = local.name-domain,
-        enableDomain = local.enable-domain,
-        enableSsms = local.enable-ssms,
-        enableHammerdb = local.enable-hammerdb,
-        enableDiskspd = local.enable-diskspd
+        nameDomain = local.nameDomain,
+        enableDomain = local.enableDomain,
+        enableSsms = local.enableSsms,
+        enableHammerdb = local.enableHammerdb,
+        enableDiskspd = local.enableDiskspd
       })
     })
   }
 
   service_account {
-    scopes = module.gce-default-scopes.scopes
+    scopes = module.gceDefaultScopes.scopes
   }
 
   allow_stopping_for_update = true
