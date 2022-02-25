57dc25832baea3e88cb566c4e8c0a283846402a4
Christoph Petersen <cp@peterschen.de>
2022-02-25 17:20:31 +0100

    Reverted name reference

--- gcp/modules/activedirectory/main.tf
+++ gcp/modules/activedirectory/main.tf
@@ -153,7 +153,7 @@ resource "google_compute_instance" "dc" {
 
   metadata = {
     sysprep-specialize-script-ps1 = templatefile(module.sysprep.path-specialize-nupkg, { 
-        nameHost = self.name, 
+        nameHost = "dc${count.index}"
         password = local.password,
         parametersConfiguration = jsonencode({
           projectName = data.google_project.project.name,
