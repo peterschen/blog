ca2a86fa28f6771dea97b96f629aecc1daf91120
Christoph Petersen <cp@peterschen.de>
2022-03-31 10:44:39 +0200

    Added script to retrieve sole-tenant node availability

---
+++ gcp/scripts/stn-availability/get-stn-availability.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -eu
+
+regions=`gcloud compute regions list --format="value(name)" | sort -u`
+types=`gcloud compute sole-tenancy node-types list --sort-by="name" --format="value(name)" | uniq`
+types_regions=`gcloud compute sole-tenancy node-types list --sort-by="zone,name" --format="value(name,zone)" | uniq`
+
+echo -en "Region"
+for type in $types; do
+    echo -en "\t$type"
+done
+
+# Add linebreak
+echo ""
+
+for region in $regions; do
+    echo -en "$region"
+
+    for type in $types; do
+        availability=0
+        available_zones=`echo "$types_regions" | grep $type | grep "$region-" | wc -l`
+
+        if [ $available_zones -gt 0 ]; then
+            availability=1
+        fi
+
+        echo -en "\t$availability"
+    done
+    
+    # Add linebreak
+    echo ""    
+done
