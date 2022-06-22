2ca009bc0562f3cbf29edf4a9a7ba5a642a400f4
Christoph Petersen <cbpetersen@google.com>
2022-06-22 10:42:01 +0200

    Added rdp sample for iap_chain

---
+++ gcp/scripts/iap-chain/iap-chainm_rdp.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env sh
+
+set +e
+
+rdp()
+{
+  POSITIONAL_ARGS=()
+
+  instance=
+  port=3389
+  zone=$ZONE
+  project=$GOOGLE_CLOUD_PROJECT
+  extra=
+
+  while [[ $# -gt 0 ]]; do
+    case $1 in
+      -p|--project)
+        project="$2"
+        shift
+        shift
+      ;;
+      -z|--zone)
+        zone="$2"
+        shift
+        shift
+      ;;
+      --)
+        shift;
+        break
+      ;;
+      -*|--*)
+        echo "Unknown option $1"
+        return 1
+      ;;
+      *)
+        POSITIONAL_ARGS+=("$1")
+        shift
+      ;;
+    esac
+  done
+
+  instance=${POSITIONAL_ARGS[0]}
+  extra="$@"
+
+  iap-chain.sh $instance $port --project $project --zone $zone -- xfreerdp +clipboard +home-drive /kbd:0x00000407 /kbd-lang:0x0407 /dynamic-resolution /log-level:WARN /v:%SERVER% $extra 
+}
