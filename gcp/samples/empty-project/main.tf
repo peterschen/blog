e10b1068adbb0ad3b533552f1773f1f76f12c168
Christoph Petersen <cbpetersen@google.com>
2023-02-21 16:26:25 +0100

    Removed network creation

--- gcp/samples/empty-project/main.tf
+++ gcp/samples/empty-project/main.tf
@@ -11,7 +11,6 @@ provider "google" {
 
 locals {
   region = var.region
-  network_range = "10.0.0.0/16"
 }
 
 module "project" {
@@ -26,19 +25,4 @@ module "project" {
     "dns.googleapis.com",
     "managedidentities.googleapis.com"
   ]
-}
-
-resource "google_compute_network" "network" {
-  project = module.project.id
-  name = module.project.id
-  auto_create_subnetworks = false
-}
-
-resource "google_compute_subnetwork" "subnetworks" {
-  project = module.project.id
-  region = local.region
-  name = local.region
-  ip_cidr_range = local.network_range
-  network = google_compute_network.network.id
-  private_ip_google_access = true
 }
