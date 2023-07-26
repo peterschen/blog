8734dfade1bbf936aec2d66499fdd4650b744b9b
Christoph Petersen <cbpetersen@google.com>
2023-07-26 09:38:46 +0200

    Updated SMSS version to 19.1

--- gcp/modules/bastion_windows/bastion.ps1
+++ gcp/modules/bastion_windows/bastion.ps1
@@ -275,7 +275,7 @@ configuration ConfigurationWorkload
             Package "InstallSsms"
             {
                 Ensure = "Present"
-                Name = "Microsoft SQL Server Management Studio 18.8"
+                Name = "Microsoft SQL Server Management Studio 19.1"
                 ProductID = ""
                 Path = "C:\Windows\temp\SSMS-Setup-ENU.exe"
                 Arguments = "/install /quiet"
