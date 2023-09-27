bdd50dc1633128f80a82e89d95c41d64b826f5c8
Christoph Petersen <cbpetersen@google.com>
2023-09-27 15:07:07 +0200

    Allow HTTP through IAP

--- gcp/modules/firewall_iap/main.tf
+++ gcp/modules/firewall_iap/main.tf
@@ -3,6 +3,10 @@ locals {
   network = var.network
   enable_rdp = var.enable_rdp
   enable_ssh = var.enable_ssh
+  enable_http = var.enable_http
+  enable_http_alt = var.enable_http_alt
+  enable_https = var.enable_https
+  enable_https_alt = var.enable_https_alt
 }
 
 resource "google_compute_firewall" "allow-rdp-iap" {
@@ -43,4 +47,84 @@ resource "google_compute_firewall" "allow-ssh-iap" {
   source_ranges = ["35.235.240.0/20"]
 
   target_tags = ["ssh"]
+}
+
+resource "google_compute_firewall" "allow-http-iap" {
+  count = local.enable_http ? 1 : 0
+  
+  project = local.project
+  name = "allow-http-iap"
+  network = local.network
+  priority = 5000
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  direction = "INGRESS"
+
+  source_ranges = ["35.235.240.0/20"]
+
+  target_tags = ["http-iap"]
+}
+
+resource "google_compute_firewall" "allow-http-alt-iap" {
+  count = local.enable_http_alt ? 1 : 0
+  
+  project = local.project
+  name = "allow-http-alt-iap"
+  network = local.network
+  priority = 5000
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8080"]
+  }
+
+  direction = "INGRESS"
+
+  source_ranges = ["35.235.240.0/20"]
+
+  target_tags = ["http-alt-iap"]
+}
+
+resource "google_compute_firewall" "allow-https-iap" {
+  count = local.enable_https ? 1 : 0
+  
+  project = local.project
+  name = "allow-https-iap"
+  network = local.network
+  priority = 5000
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  direction = "INGRESS"
+
+  source_ranges = ["35.235.240.0/20"]
+
+  target_tags = ["https-iap"]
+}
+
+resource "google_compute_firewall" "allow-https-alt-iap" {
+  count = local.enable_https_alt ? 1 : 0
+  
+  project = local.project
+  name = "allow-https-alt-iap"
+  network = local.network
+  priority = 5000
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8443"]
+  }
+
+  direction = "INGRESS"
+
+  source_ranges = ["35.235.240.0/20"]
+
+  target_tags = ["https-alt-iap"]
 }
--- gcp/modules/firewall_iap/variables.tf
+++ gcp/modules/firewall_iap/variables.tf
@@ -15,3 +15,23 @@ variable "enable_ssh" {
     type = bool
     default = true
 }
+
+variable "enable_http" {
+    type = bool
+    default = false
+}
+
+variable "enable_http_alt" {
+    type = bool
+    default = false
+}
+
+variable "enable_https" {
+    type = bool
+    default = false
+}
+
+variable "enable_https_alt" {
+    type = bool
+    default = false
+}
