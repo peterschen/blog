39d8e8d74ef4f22668eb46ed2f281876e022abf7
Christoph Petersen <cbpetersen@google.com>
2024-02-16 11:24:21 +0000

    Updated variables to not include optional attributes

--- gcp/projects/springboard/terraform/modules/springboard_policy/variables.tf
+++ gcp/projects/springboard/terraform/modules/springboard_policy/variables.tf
@@ -8,9 +8,9 @@ variable "constraints" {
         object({
             constraint = string
             type = string
-            enforce = optional(bool)
-            allowed_values = optional(list(string))
-            denied_values = optional(list(string))
+            enforce = bool
+            allowed_values = list(string)
+            denied_values = list(string)
         })
     )
     default = []
