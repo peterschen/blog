fab0fca8d8650292094ee6e37c245ae91ff2762b
Christoph Petersen <cbpetersen@google.com>
2024-02-26 16:36:20 +0000

    Added outputs

---
+++ gcp/projects/springboard/modules/springboard_core/outputs.tf
@@ -0,0 +1,19 @@
+output "project_id" {
+  value = module.project.id
+}
+
+output "project_name" {
+  value = module.project.name
+}
+
+output "project_number" {
+  value = module.project.number
+}
+
+output "network_id" {
+  value = module.network.id
+}
+
+output "network_name" {
+  value = module.network.name
+}
---
+++ gcp/projects/springboard/springboard_tier1/outputs.tf
@@ -0,0 +1,19 @@
+output "project_id" {
+  value = module.springboard.project_id
+}
+
+output "project_name" {
+  value = module.springboard.project_name
+}
+
+output "project_number" {
+  value = module.springboard.project_number
+}
+
+output "network_id" {
+  value = module.springboard.network_id
+}
+
+output "network_name" {
+  value = module.springboard.network_name
+}
