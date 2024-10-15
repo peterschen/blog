d0b7c5aa3b1240bd4360347ee0471b07ff3e0b2a
Christoph Petersen <cbpetersen@google.com>
2024-10-15 18:24:44 +0000

    Bumped SqlServerDsc to 17.0.0

--- gcp/modules/sqlserver/main.tf
+++ gcp/modules/sqlserver/main.tf
@@ -155,7 +155,7 @@ resource "google_compute_instance" "sql" {
             },
             { 
               Name = "SqlServerDsc",
-              Version = "16.0.0"
+              Version = "17.0.0"
             },
             { 
               Name = "StorageDsc",
