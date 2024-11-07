b8f60b1a000218a6d0ef01520e06d735771c5fc1
Christoph Petersen <cbpetersen@google.com>
2024-11-07 19:05:30 +0000

    Fixed missing credential creation

--- gcp/projects/pass24/demo2_customization-sql-1.ps1
+++ gcp/projects/pass24/demo2_customization-sql-1.ps1
@@ -40,4 +40,41 @@ configuration Customization
         PsDscRunAsCredential = $Credential
         DependsOn = "[WaitForAll]SqlServerSetup"
     }
+
+    Script "CreateCredential"
+    {
+        GetScript = {
+            $result = Invoke-Sqlcmd -Query "SELECT name FROM sys.credentials WHERE credential_identity = 'S3 Access Key'" -ServerInstance "sql-0";
+            if($result -ne $null)
+            {
+                $result = "Present";
+            }
+            else
+            {
+                $result = "Absent";
+            }
+            
+            return @{Ensure = $result};
+        }
+
+        TestScript = {
+            $state = [scriptblock]::Create($GetScript).Invoke();
+            return $state.Ensure -eq "Present";
+        }
+
+        SetScript = {
+            $secret = gcloud secrets versions access 1 --secret pass24-gcs-access --project cbpetersen-shared;
+            $query = @"
+-- Configure credential for GCS
+IF NOT EXISTS (SELECT * FROM sys.credentials WHERE credential_identity = 'S3 Access Key')
+    CREATE CREDENTIAL [s3://storage.googleapis.com/pass-demo-2024]
+    WITH
+        IDENTITY = 'S3 Access Key',
+        SECRET = '${secret}';
+"@;
+            Invoke-Sqlcmd -Query $query -ServerInstance "sql-0";
+        }
+
+        DependsOn = "[SqlSetup]SqlServerSetup"
+    }
 }
