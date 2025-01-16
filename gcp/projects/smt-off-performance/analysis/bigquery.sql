b8494816e489971ecd31b9ef2addbadc49fb609d
Christoph Petersen <cbpetersen@google.com>
2025-01-16 11:04:30 +0000

    Exclude duplicate data for reporting

--- gcp/projects/smt-off-performance/analysis/bigquery.sql
+++ gcp/projects/smt-off-performance/analysis/bigquery.sql
@@ -1,49 +1,57 @@
 CREATE OR REPLACE VIEW smtoff.perfcounters_v
 AS (
   SELECT
-    run,
-    sku,
-    users,
-    `timestamp`,
-    path,
-    value
-  FROM (
+    *,
+    RANK() OVER (PARTITION BY run, sku, users, `timestamp`, `path` ORDER BY `timestamp` DESC) AS rnk
+  FROM 
+  (
     SELECT
       run,
       sku,
       users,
-      MIN(`timestamp`) AS start,
-      MAX(`timestamp`) AS finish
-    FROM smtoff.jobs
-    GROUP BY 1, 2, 3
-  ) AS j
-  INNER JOIN (
+      `timestamp`,
+      `path`,
+      `value`
+    FROM (
+      SELECT
+        run,
+        sku,
+        users,
+        MIN(`timestamp`) AS start,
+        MAX(`timestamp`) AS finish
+      FROM smtoff.jobs
+      GROUP BY 1, 2, 3
+    ) AS j
+    INNER JOIN (
+      SELECT
+        -- PARSE_TIMESTAMP('%m/%d/%Y %I:%M:%S %P', `timestamp`) AS `timestamp`,
+        -- PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%S%Ez', `timestamp`) AS `timestamp`,
+        `timestamp`,
+        REGEXP_REPLACE(path, r'\([0-9]{1,2} ([a-z]{1}:)\)', r'(\1)') path,
+        value
+      FROM
+        smtoff.perfcounters
+    ) AS p
+    ON (
+      p.timestamp BETWEEN j.start AND j.finish
+    )
+    UNION ALL
     SELECT
-      PARSE_TIMESTAMP('%m/%d/%Y %I:%M:%S %P', `timestamp`) AS `timestamp`,
-      REGEXP_REPLACE(path, r'\([0-9]{1,2} ([a-z]{1}:)\)', r'(\1)') path,
-      value
-    FROM
-      smtoff.perfcounters
-  ) AS p
-  ON (
-    p.timestamp BETWEEN j.start AND j.finish
+      run,
+      sku,
+      users,
+      `timestamp`,
+      'tpm' AS path,
+      counter AS value
+    FROM smtoff.counters AS c
+    INNER JOIN (
+      SELECT
+        jobid,
+        users
+      FROM
+        smtoff.jobs
+    ) AS j ON c.jobid = j.jobid
   )
-  UNION ALL
-  SELECT
-    run,
-    sku,
-    users,
-    `timestamp`,
-    'tpm' AS path,
-    counter AS value
-  FROM smtoff.counters AS c
-  INNER JOIN (
-    SELECT
-      jobid,
-      users
-    FROM
-      smtoff.jobs
-  ) AS j ON c.jobid = j.jobid
 )
 ;
 
@@ -60,6 +68,8 @@ AS (
     APPROX_QUANTILES(value, 100)[OFFSET(90)] AS value_90
   FROM
     smtoff.perfcounters_v
+  WHERE users IS NOT NULL
+  AND rnk = 1
   GROUP BY 1, 2, 3, 4    
 )
 ;
