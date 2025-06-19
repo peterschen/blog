c21c7ed580d74244137ba4c7ae7bbee591ecc89b
Christoph Petersen <cbpetersen@google.com>
2025-06-19 12:10:02 +0000

    Terraform demo module for PASS25

---
+++ gcp/projects/pass25/demo/outputs.tf
@@ -0,0 +1,19 @@
+output "project_id" {
+  value = data.google_project.project.project_id
+}
+
+output "zones" {
+    value = local.zones
+}
+
+output "network_id" {
+  value = google_compute_network.network.id
+}
+
+output "network_name" {
+  value = google_compute_network.network.name
+}
+
+output "instances" {
+    value = module.sqlserver.instances
+}
---
+++ gcp/projects/pass25/demo/variables.tf
@@ -0,0 +1,84 @@
+variable "project_id" {
+  type = string
+  default = null
+}
+
+variable "org_id" {
+  type = number
+  default = null
+}
+
+variable "billing_account" {
+  type = string
+  default = null
+}
+
+variable "prefix" {
+  type = string
+  default = null
+}
+
+variable "region" {
+  type = string
+  default = "europe-west4"
+}
+
+variable "zones" {
+  type = list(string)
+  default = [
+    "europe-west4-a",
+    "europe-west4-b"
+  ]
+}
+
+variable "domain_name" {
+}
+
+variable "password" {
+  sensitive = true
+}
+
+variable "machine_type_bastion" {
+  type = string
+  default = "n4-highcpu-4"
+}
+
+variable "machine_type_sql" {
+  type = string
+  default = "n4-standard-4"
+}
+
+variable "windows_image" {
+  type = string
+  default = "windows-cloud/windows-2022"
+}
+
+variable "windows_core_image" {
+  type = string
+  default = "windows-cloud/windows-2022-core"
+}
+
+variable "sql_image" {
+  type = string
+  default = "windows-cloud/windows-2022"
+}
+
+variable "enable_cluster" {
+  type = bool
+  default = false
+}
+
+variable "enable_alwayson" {
+  type = bool
+  default = false
+}
+
+variable "enable_iam" {
+  type = bool
+  default = true
+}
+
+variable "configuration_customization" {
+  type = list(string)
+  default = []
+}
