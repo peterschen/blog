f311d7cc742dd56f492efa23ab5f3f296c089609
Christoph Petersen <cbpetersen@google.com>
2025-06-27 07:12:36 +0000

    Updated demo4 customizations

--- gcp/projects/pass25/demo4_customization-sql-0.ps1
+++ gcp/projects/pass25/demo4_customization-sql-0.ps1
@@ -81,25 +81,14 @@ configuration Customization
         }
 
         SetScript = {
-            $secret = gcloud secrets versions access 1 --secret pass25-demo-gcs --project cbpetersen-shared;
+            $secret = gcloud secrets versions access 1 --secret pass-demo-gcs --project cbpetersen-shared;
             $query = @"
 -- Server configuration
 EXEC sp_configure 'show advanced options', 1;
 RECONFIGURE;
--- EXEC sp_configure 'max degree of parallelism', 8;
--- EXEC sp_configure 'max worker threads', 6000;
--- EXEC sp_configure 'min server memory', 0;
--- EXEC sp_configure 'max server memory', 131072;
--- EXEC sp_configure 'recovery interval (min)', 15;
--- EXEC sp_configure 'lightweight pooling', 1;
--- EXEC sp_configure 'priority boost', 1;
+EXEC sp_configure 'max server memory', 131072;
+EXEC sp_configure 'recovery interval (min)', 15;
 RECONFIGURE;
-
--- Enable checkpoint tracing
-DBCC TRACEON (3502, -1);
-DBCC TRACEON (3504, -1);
-DBCC TRACEON (3605, -1);
-GO
 
 -- Configure credential for GCS
 IF NOT EXISTS (SELECT * FROM sys.credentials WHERE credential_identity = 'S3 Access Key')
@@ -112,26 +101,28 @@ IF NOT EXISTS (SELECT * FROM sys.credentials WHERE credential_identity = 'S3 Acc
         }
     }
 
-    SqlScriptQuery "RestoreDatabase1"
+    for($i = 0; $i -lt 3; $i++)
     {
-        Id = "RestoreDatabase1"
-        ServerName = "sql-0"
-        InstanceName = "MSSQLSERVER"
+        SqlScriptQuery "RestoreDatabase${i}"
+        {
+            Id = "RestoreDatabase${i}"
+            ServerName = "sql-0"
+            InstanceName = "MSSQLSERVER"
 
-        TestQuery = @"
-IF (SELECT COUNT(name) FROM sys.databases WHERE name = 'demo4_1') = 0
+            TestQuery = @"
+IF (SELECT COUNT(name) FROM sys.databases WHERE name = 'demo4_${i}') = 0
 BEGIN
-    RAISERROR ('Did not find database [demo4_1]', 16, 1)
+    RAISERROR ('Did not find database [demo4_${i}]', 16, 1)
 END
 ELSE
 BEGIN
-    PRINT 'Found database [demo4_1]'
+    PRINT 'Found database [demo4_${i}]'
 END
 "@
-        GetQuery = "SELECT name FROM sys.databases WHERE name = 'demo4_1'"
-        SetQuery = @"
+            GetQuery = "SELECT name FROM sys.databases WHERE name = 'demo4_${i}'"
+            SetQuery = @"
 -- Restore database
-RESTORE DATABASE [demo4_1]
+RESTORE DATABASE [demo4_${i}]
 FROM
     URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_01.bak',
     URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_02.bak',
@@ -198,125 +189,18 @@ FROM
     URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_63.bak',
     URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_64.bak'
 WITH 
-    MOVE 'demo4' TO 'T:\demo4_1.mdf',
-    MOVE 'demo4_log' TO 'T:\demo4_1_log.ldf',
+    MOVE 'demo4' TO 'T:\demo4_${i}.mdf',
+    MOVE 'demo4_log' TO 'T:\demo4_${i}_log.ldf',
     STATS = 10, 
     RECOVERY,
     REPLACE;
 GO
 
--- ALTER DATABASE [demo4_1] MODIFY FILE ( NAME = N'demo4_log', SIZE = 128GB, FILEGROWTH = 0)
-ALTER DATABASE [demo4_1] SET RECOVERY SIMPLE;
--- ALTER DATABASE [demo4_1] SET TORN_PAGE_DETECTION OFF;
--- ALTER DATABASE [demo4_1] SET PAGE_VERIFY NONE;
--- ALTER DATABASE [demo4_1] SET TARGET_RECOVERY_TIME = 15 MINUTES;
-GO
+ALTER DATABASE [demo4_${i}] SET RECOVERY FULL;
+ALTER DATABASE [demo4_${i}] SET TARGET_RECOVERY_TIME = 15 MINUTES;
 "@;
-        DependsOn = "[Script]ConfigureDatabase"
-        PsDscRunAsCredential = $Credential
-    }
-
-    SqlScriptQuery "RestoreDatabase2"
-    {
-        Id = "RestoreDatabase2"
-        ServerName = "sql-0"
-        InstanceName = "MSSQLSERVER"
-
-        TestQuery = @"
-IF (SELECT COUNT(name) FROM sys.databases WHERE name = 'demo4_2') = 0
-BEGIN
-    RAISERROR ('Did not find database [demo4_2]', 16, 1)
-END
-ELSE
-BEGIN
-    PRINT 'Found database [demo4_2]'
-END
-"@
-        GetQuery = "SELECT name FROM sys.databases WHERE name = 'demo4_2'"
-        SetQuery = @"
--- Restore database
-RESTORE DATABASE [demo4_2]
-FROM
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_01.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_02.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_03.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_04.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_05.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_06.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_07.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_08.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_09.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_10.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_11.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_12.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_13.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_14.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_15.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_16.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_17.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_18.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_19.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_20.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_21.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_22.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_23.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_24.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_25.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_26.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_27.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_28.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_29.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_30.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_31.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_32.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_33.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_34.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_35.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_36.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_37.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_38.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_39.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_40.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_41.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_42.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_43.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_44.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_45.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_46.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_47.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_48.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_49.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_50.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_51.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_52.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_53.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_54.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_55.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_56.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_57.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_58.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_59.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_60.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_61.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_62.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_63.bak',
-    URL = 's3://storage.googleapis.com/cbpetersen-demos/pass25/demo4_2500_64.bak'
-WITH 
-    MOVE 'demo4' TO 'T:\demo4_2.mdf',
-    MOVE 'demo4_log' TO 'T:\demo4_2_log.ldf',
-    STATS = 10, 
-    RECOVERY,
-    REPLACE;
-GO
-
--- ALTER DATABASE [demo4_2] MODIFY FILE ( NAME = N'demo4_log', SIZE = 128GB, FILEGROWTH = 0)
-ALTER DATABASE [demo4_2] SET RECOVERY SIMPLE;
--- ALTER DATABASE [demo4_2] SET TORN_PAGE_DETECTION OFF;
--- ALTER DATABASE [demo4_2] SET PAGE_VERIFY NONE;
--- ALTER DATABASE [demo4_2] SET TARGET_RECOVERY_TIME = 15 MINUTES;
-GO
-"@;
-        DependsOn = "[Script]ConfigureDatabase"
-        PsDscRunAsCredential = $Credential
+            DependsOn = "[Script]ConfigureDatabase"
+            PsDscRunAsCredential = $Credential
+        }
     }
 }
