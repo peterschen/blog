3ba9ce026d7f6bad6b7ca1540ef90ad9c6df6952
Christoph Petersen <cbpetersen@google.com>
2025-11-14 19:49:32 +0000

    Finished demo2

--- gcp/projects/pass25/demo2.tf
+++ gcp/projects/pass25/demo2.tf
@@ -16,6 +16,7 @@ module "demo2" {
   domain_name = local.domain_name
   password = var.password
   enable_cluster = true
+  enable_quorum = false
   enable_iam = false
 
   machine_type_bastion = "n4-highcpu-4"
@@ -27,66 +28,257 @@ module "demo2" {
   ]
 }
 
-resource "google_compute_disk" "demo2_data" {
+data "google_compute_network" "demo2" {
+  count  = local.enable_demo2 ? 1 : 0
+  project = module.demo2[count.index].project_id
+  name = module.demo2[count.index].network_name
+}
+
+data "google_compute_subnetwork" "demo2" {
+  count  = local.enable_demo2 ? 1 : 0
+  project = module.demo2[count.index].project_id
+  region = local.region_demo2
+  name = local.region_demo2
+}
+
+resource "google_compute_subnetwork" "demo2" {
+  count  = local.enable_demo2 ? 1 : 0
+  project = module.demo2[count.index].project_id
+  region = local.region_demo2
+  name = "demo2"
+  ip_cidr_range = "10.1.0.0/24"
+  network = data.google_compute_network.demo2[count.index].id
+  purpose = "REGIONAL_MANAGED_PROXY"
+  role = "ACTIVE"
+}
+
+resource "google_compute_firewall" "demo2_healthcheck" {
+  count  = local.enable_demo2 ? 1 : 0
+  project = module.demo2[count.index].project_id
+
+  name = "allow-mssql-healthcheck"
+  network = data.google_compute_network.demo2[count.index].self_link
+  priority = 5000
+
+  allow {
+    protocol = "tcp"
+    ports    = ["1433"]
+  }
+
+  direction = "INGRESS"
+
+  source_ranges = ["35.191.0.0/16", "209.85.152.0/22", "209.85.204.0/22"]
+  target_tags = ["mssql"]
+}
+
+resource "google_compute_firewall" "demo2_loadbalancer" {
+  count  = local.enable_demo2 ? 1 : 0
+  project = module.demo2[count.index].project_id
+
+  name = "allow-mssql-loadbalancer"
+  network = data.google_compute_network.demo2[count.index].self_link
+  priority = 5000
+
+  allow {
+    protocol = "tcp"
+    ports    = ["1433"]
+  }
+
+  direction = "INGRESS"
+
+  source_ranges = [
+    google_compute_subnetwork.demo2[count.index].ip_cidr_range
+  ]
+  target_tags = ["mssql"]
+}
+
+# data "google_compute_instance_group" "demo2_primary" {
+#   count = local.enable_demo2 ? 1 : 0
+#   project = module.demo2[count.index].project_id
+#   zone = local.zone_demo2
+#   name = "wsfc-sql-0"
+# }
+
+# data "google_compute_instance_group" "demo2_secondary" {
+#   count = local.enable_demo2 ? 1 : 0
+#   project = module.demo2[count.index].project_id
+#   zone = local.zone_secondary_demo2
+#   name = "wsfc-sql-1"
+# }
+
+resource "google_compute_region_health_check" "demo2" {
+  count = local.enable_demo2 ? 1 : 0
+  project = module.demo2[count.index].project_id
+  region = local.region_demo2
+
+  name = "sql"
+  timeout_sec = 1
+  check_interval_sec = 1
+  healthy_threshold = 1
+  unhealthy_threshold = 1
+
+  tcp_health_check {
+    port = 1433
+  }
+}
+
+resource "google_compute_network_endpoint_group" "demo2" {
+  count = local.enable_demo2 ? 1 : 0
+  project = module.demo2[count.index].project_id
+  zone = local.zone_demo2
+
+  name = "sql"
+  network_endpoint_type = "NON_GCP_PRIVATE_IP_PORT"
+  network = data.google_compute_network.demo2[count.index].self_link
+}
+
+data "google_compute_address" "wsfc_sql" {
+  count = local.enable_demo2 ? 1 : 0
+  project = module.demo2[count.index].project_id
+  region = local.region_demo2
+  name = "wsfc-sql"
+}
+
+resource "google_compute_network_endpoint" "demo2" {
+  count = local.enable_demo2 ? 1 : 0
+  project = module.demo2[count.index].project_id
+  zone = local.zone_demo2
+  network_endpoint_group = google_compute_network_endpoint_group.demo2[count.index].id
+  ip_address = data.google_compute_address.wsfc_sql[count.index].address
+  port = 1433
+}
+
+resource "google_compute_region_backend_service" "demo2" {
+  count = local.enable_demo2 ? 1 : 0
+  project = module.demo2[count.index].project_id
+  region = local.region_demo2
+
+  name = "sql"
+
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+  ip_address_selection_policy = "IPV4_ONLY"
+  protocol = "TCP"
+  port_name = "mssql"
+
+  health_checks = [
+    google_compute_region_health_check.demo2[count.index].id
+  ]
+  
+  dynamic "backend" {
+    for_each = google_compute_network_endpoint_group.demo2
+    content {
+      balancing_mode = "CONNECTION"
+      capacity_scaler = 1
+      group = backend.value.id
+      max_connections_per_endpoint = 10000
+    }
+  }
+
+  log_config {
+    enable = true
+    optional_mode = "INCLUDE_ALL_OPTIONAL"
+  }
+}
+
+resource "google_compute_region_target_tcp_proxy" "demo2" {
+  count = local.enable_demo2 ? 1 : 0
+  project = module.demo2[count.index].project_id
+  region = local.region_demo2
+
+  name = "sql"
+  backend_service = google_compute_region_backend_service.demo2[count.index].id
+}
+
+resource "google_compute_address" "demo2" {
+  count = local.enable_demo2 ? 1 : 0
+  project = module.demo2[count.index].project_id
+  region = local.region_demo2
+
+  name = "sql"
+  address_type = "EXTERNAL"
+}
+
+resource "google_compute_forwarding_rule" "demo2" {
+  count = local.enable_demo2 ? 1 : 0
+  project = module.demo2[count.index].project_id
+  region = local.region_demo2
+
+  name = "sql"
+  ip_address = google_compute_address.demo2[count.index].address
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+  ip_protocol = "TCP"
+  port_range = "1433-1433"
+  target = google_compute_region_target_tcp_proxy.demo2[count.index].id
+  network = data.google_compute_network.demo2[count.index].id
+
+  depends_on = [
+    google_compute_subnetwork.demo2
+  ]
+}
+
+resource "google_compute_region_disk" "demo2_data" {
   provider = google-beta
   count = local.enable_demo2 ? 1 : 0
   project = module.demo2[0].project_id
-  zone = local.zone_demo2
+  region = local.region_demo2
+  replica_zones = [
+    local.zone_demo2,
+    local.zone_secondary_demo2
+  ]
   name = "data"
-  type = "hyperdisk-balanced"
+  type = "hyperdisk-balanced-high-availability"
   access_mode = "READ_WRITE_MANY"
   size = 100
   provisioned_iops = 3000
   provisioned_throughput = 140
 }
 
-# resource "google_compute_region_disk" "demo2_quorum" {
-#   provider = google-beta
-#   count = local.enable_demo2 ? 1 : 0
-#   project = module.demo2[0].project_id
-#   region = local.region_demo2
-#   replica_zones = [
-#     local.zone_demo2,
-#     # local.zone_secondary_demo2
-#     "europe-west4-b"
-#   ]
-#   name = "quorum"
-#   type = "hyperdisk-balanced-high-availability"
-#   size = 4
-#   access_mode = "READ_WRITE_MANY"
-#   provisioned_iops = 2000
-#   provisioned_throughput = 140
-# }
+resource "google_compute_region_disk" "demo2_quorum" {
+  provider = google-beta
+  count = local.enable_demo2 ? 1 : 0
+  project = module.demo2[0].project_id
+  region = local.region_demo2
+  replica_zones = [
+    local.zone_demo2,
+    local.zone_secondary_demo2
+  ]
+  name = "quorum"
+  type = "hyperdisk-balanced-high-availability"
+  size = 4
+  access_mode = "READ_WRITE_MANY"
+  provisioned_iops = 2000
+  provisioned_throughput = 140
+}
 
 resource "google_compute_attached_disk" "demo2_data" {
   for_each = {
     for entry in flatten([
       for module in module.demo2: [
-        for instance in module.instances: instance
+        for instance in nonsensitive(module.instances): instance
       ]
     ]): "${entry.name}" => entry
   }
 
   project = module.demo2[0].project_id
-  disk = google_compute_disk.demo2_data[0].id
+  disk = google_compute_region_disk.demo2_data[0].id
   instance = each.value.id
-  device_name = google_compute_disk.demo2_data[0].name
+  device_name = google_compute_region_disk.demo2_data[0].name
 }
 
-# resource "google_compute_attached_disk" "demo2_quorum" {
-#   for_each = {
-#     for entry in flatten([
-#       for module in module.demo2: [
-#         for instance in module.instances: instance
-#       ]
-#     ]): "${entry.name}" => entry
-#   }
+resource "google_compute_attached_disk" "demo2_quorum" {
+  for_each = {
+    for entry in flatten([
+      for module in module.demo2: [
+        for instance in nonsensitive(module.instances): instance
+      ]
+    ]): "${entry.name}" => entry
+  }
 
-#   project = module.demo2[0].project_id
-#   disk = google_compute_region_disk.demo2_quorum[0].id
-#   instance = each.value.id
-#   device_name = google_compute_region_disk.demo2_quorum[0].name
-# }
+  project = module.demo2[0].project_id
+  disk = google_compute_region_disk.demo2_quorum[0].id
+  instance = each.value.id
+  device_name = google_compute_region_disk.demo2_quorum[0].name
+}
 
 data "google_compute_default_service_account" "default_demo2" {
   count = local.enable_demo2 ? 1 : 0
--- gcp/projects/pass25/demo2_customization-sql-0.ps1
+++ gcp/projects/pass25/demo2_customization-sql-0.ps1
@@ -15,15 +15,13 @@ configuration Customization
     $agentCredential = New-Object System.Management.Automation.PSCredential ("$($Parameters.domainName)\s-SqlAgent", $Credential.Password);
     $engineCredential = New-Object System.Management.Automation.PSCredential ("$($Parameters.domainName)\s-SqlEngine", $Credential.Password);
 
-    # TODO Christoph: Configure quorum!
-
     Script "InitDisk"
     {
         GetScript = {
             # Ensure status of disks is current
             Get-PhysicalDisk | Reset-PhysicalDisk;
             
-            $disk = Get-PhysicalDisk -CanPool $true;
+            $disk = Get-PhysicalDisk -CanPool $true | Where-Object -Property Size -EQ -Value 100GB | Select-Object -First 1;
             if($disk -eq $null)
             {
                 $result = "Present";
@@ -42,11 +40,45 @@ configuration Customization
         }
 
         SetScript = {
-            $disk = Get-PhysicalDisk -CanPool $true;
-
+            $disk = Get-PhysicalDisk -CanPool $true | Where-Object -Property Size -EQ -Value 100GB | Select-Object -First 1;
+            
             # Initialize disk
             Initialize-Disk -UniqueId $disk.UniqueId -PassThru |
                 New-Partition -DriveLetter "T" -UseMaximumSize | 
+                Format-Volume;
+        }
+    }
+
+    Script "InitQuorumDisk"
+    {
+        GetScript = {
+            # Ensure status of disks is current
+            Get-PhysicalDisk | Reset-PhysicalDisk;
+            
+            $disk = Get-disk | Where-Object -Property Size -EQ -Value 4GB | Where-Object -Property PartitionStyle -EQ -Value "RAW" |  Select-Object -First 1;
+            if($disk -eq $null)
+            {
+                $result = "Present";
+            }
+            else
+            {
+                $result = "Absent";
+            }
+            
+            return @{Ensure = $result};
+        }
+
+        TestScript = {
+            $state = [scriptblock]::Create($GetScript).Invoke();
+            return $state.Ensure -eq "Present";
+        }
+
+        SetScript = {
+            $disk = Get-disk | Where-Object -Property Size -EQ -Value 4GB | Where-Object -Property PartitionStyle -EQ -Value "RAW" |  Select-Object -First 1;
+            
+            # Initialize disk
+            Initialize-Disk -UniqueId $disk.UniqueId -PassThru |
+                New-Partition -DriveLetter "Q" -UseMaximumSize | 
                 Format-Volume;
         }
     }
@@ -74,7 +106,7 @@ configuration Customization
 
         SetScript = {
             # Add disk to cluster
-            $clusterDisk = Get-ClusterAvailableDisk | Add-ClusterDisk;
+            $clusterDisk = Get-ClusterAvailableDisk | Where-Object -Property Size -EQ -Value 100GB | Add-ClusterDisk;
             $resource = Add-ClusterSharedVolume -Name $clusterDisk.Name;
 
             # Bring disk online if necessary
@@ -85,12 +117,44 @@ configuration Customization
         DependsOn = "[Script]InitDisk"
     }
 
+    Script "AddClusterQuorumDisk"
+    {
+        GetScript = {
+            $resource = Get-ClusterQuorum -ErrorAction "SilentlyContinue";
+            if($resource -ne $null -and $resource.QuorumResource -ne $null -and $resource.QuorumResource.ResourceType -eq "Physical Disk")
+            {
+                $result = "Present";
+            }
+            else
+            {
+                $result = "Absent";
+            }
+            
+            return @{Ensure = $result};
+        }
+
+        TestScript = {
+            $state = [scriptblock]::Create($GetScript).Invoke();
+            return $state.Ensure -eq "Present";
+        }
+
+        SetScript = {
+            $clusterDisk = Get-ClusterAvailableDisk | Where-Object -Property Size -EQ -Value 4GB | Add-ClusterDisk;
+            Set-ClusterQuorum -DiskWitness $clusterDisk.Name;
+        }
+
+        PsDscRunAsCredential = $Credential
+        DependsOn = "[Script]InitQuorumDisk"
+    }
+
     SqlSetup "SqlServerSetup"
     {
         Action = "InstallFailoverCluster"
         SourcePath = "C:\sql_server_install"
         Features = "SQLENGINE,FULLTEXT"
         InstanceName = "MSSQLSERVER"
+        SecurityMode = "SQL"
+        SAPwd = $domainCredential
         SQLSysAdminAccounts = "$($Parameters.domainName)\g-SqlAdministrators"
         SQLSvcAccount = $engineCredential
         AgtSvcAccount = $agentCredential
