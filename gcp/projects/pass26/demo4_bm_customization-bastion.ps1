c88512211647ee13377fd6c43111023a64e65adc
Christoph Petersen <cbpetersen@google.com>
2026-07-08 10:04:37 +0000

    Updated benchmark script

--- gcp/projects/pass26/demo4_bm_customization-bastion.ps1
+++ gcp/projects/pass26/demo4_bm_customization-bastion.ps1
@@ -21,19 +21,23 @@ configuration Customization
 #!/bin/tclsh
 
 proc wait_to_complete { seconds } {
-set x 0
-set timerstop 0
-while {!`$timerstop} {
-incr x
-after 1000
-if { ![ expr {`$x % 60} ] } {
-set y [ expr `$x / 60 ]
-puts "Timer: `$y minutes elapsed"
-}
-update
-if {  [ vucomplete ] || `$x eq `$seconds } { set timerstop 1 }
-}
-return
+    set x 0
+    set timerstop 0
+    while {!`$timerstop} {
+        incr x
+        after 1000
+        
+        if { ![ expr {`$x % 60} ] } {
+            set y [ expr `$x / 60 ]
+            puts "Timer: `$y minutes elapsed"
+        }
+        
+        update
+        
+        if {  [ vucomplete ] || `$x eq `$seconds } { set timerstop 1 }
+    }
+    
+    return
 }
 
 puts "SETTING CONFIGURATION"
@@ -53,25 +57,31 @@ diset tpcc mssqls_allwarehouse true
 diset tpcc mssqls_timeprofile false
 diset tpcc mssqls_dbase demo4_${i}
 diset tpcc mssqls_driver timed
-diset tpcc mssqls_rampup 2
+diset tpcc mssqls_rampup 3
 diset tpcc mssqls_duration 10
 diset tpcc mssqls_checkpoint true
 
 vuset logtotemp 1
 vuset nobuff 1
-vuset delay 100
+vuset delay 220
+
+tcset logtotemp 1
+tcset refreshrate 10
 
 puts "Loading script"
 loadscript
 
 puts "TEST SEQUENCE STARTED"
 vudestroy
+tcstop
 puts "704 VU TEST"
 vuset vu 704
 vucreate
+tcstart
 vurun
 
-wait_to_complete 1500
+wait_to_complete 5400
+tcstop
 vudestroy
 
 puts "TEST SEQUENCE COMPLETE"
@@ -88,6 +98,25 @@ Set-Location -Path `$pathHammerdb;
 
 # Start run
 .\hammerdbcli auto `$pathTools/pass_run_${i}.tcl
+"@
+    }
+
+    File "BmRunner${i}"
+    {
+        DestinationPath = "C:\tools\bm_$i.ps1"
+        Contents = @"
+`$query = @`"
+USE [demo4_${i}];
+CHECKPOINT
+GO
+`"@
+
+for(`$i = 0; `$i -lt 6; `$i++) { 
+    Invoke-Sqlcmd -Query `$query -ServerInstance "sql-0" -TrustServerCertificate -ErrorAction Stop;
+
+    cd \tools;
+    .\pass_run_${i}.ps1; 
+}
 "@
     }
 }
